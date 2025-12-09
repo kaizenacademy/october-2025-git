54601ba17555548dcd04a123f573c9f7cc8565fb
EC2 Default User <ec2-user@ip-172-31-74-103.ec2.internal>
2025-12-09 01:51:36 +0000

    added 3rd class files

---
+++ class3/and.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter your age: " age
+
+if [ $age -le 0 ]
+then
+	echo "Enter right age"
+elif [ $age -lt 13 ] && [ $age -gt 0 ]
+then
+	echo "Child"
+elif [ $age -ge 13 ] && [ $age -lt 18 ]  	# 13-18
+then
+	echo "Teenager"
+elif [ $age -ge 18 ] && [ $age -lt 65 ]
+then
+	echo "Adult"
+else
+	echo "Elder"
+fi
